f3931e6cf7961187b5c5b2a7df1ee37765a0dd48
IllusiveBagel <logan@neutral.systems>
2022-05-27 14:11:02 +0100

    Added Get script and updated files

---
+++ get.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Get the i3 config
+echo Copying I3 Config...
+cp ~/.config/i3/config ./i3/
+cp ~/.config/i3/polybar.sh ./i3/
+echo I3 Copied!
+
+# Get the polybar config
+echo Copying Polybar Config...
+cp ~/.config/polybar/config ./polybar/
+echo Polybar Copied!
+
+# Get the Vim config
+echo Copying Vim Config...
+cp ~/.vimrc ./vim/
+echo Vim Copied!
+
+# Get zsh config
+echo Copying Zsh Config...
+cp ~/.zshrc ./zsh/
+echo Zsh Copied!
+
+# Get XResources
+echo Copying XResources
+cp ~/.Xresources ./
+echo XResources Copied!
