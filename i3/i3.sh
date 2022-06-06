9376959a644c257ae7809a5c909b222d6b2e9659
IllusiveBagel <logan@neutral.systems>
2022-06-06 10:31:39 +0100

    Added Portable Polybar Logic

--- i3/i3.sh
+++ i3/i3.sh
@@ -2,7 +2,7 @@
 
 SECONDARY=DP-2
 
-if $(xrandr --prop | grep -q "$Secondary connected");then
+if $(xrandr --prop | grep -q "$SECONDARY connected");then
 	cp ~/.config/i3/desktop.config ~/.config/i3/config
 else
 	cp ~/.config/i3/laptop.config ~/.config/i3/config
--- i3/polybar.sh
+++ i3/polybar.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+SECONDARY=DP-2
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -7,5 +9,9 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar primary &
-polybar secondary &
+if $(xrandr --prop | grep -q "$SECONDARY connected");then
+	polybar primary &
+	polybar secondary &
+else
+	polybar single &
+fi
